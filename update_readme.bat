9552ee11a2845e5ffa6399638656b15388e5efd8
NikolaJacques <44374495+NikolaJacques@users.noreply.github.com>
2023-03-23 01:04:27 +0100

    removed bat files

--- update_readme.bat
+++
@@ -1,5 +0,0 @@
-git remote set-url --add --push origin git@github.com:NikolaJacques/error-logger-shared-types.git
-git remote set-url --add --push origin git@github.com:NikolaJacques/error-logger-back-end.git
-git remote set-url --add --push origin git@github.com:NikolaJacques/error-logger-front-end.git
-git remote set-url --add --push origin git@github.com:NikolaJacques/error-logger-monday-module.git
-git remote set-url --add --push origin git@github.com:NikolaJacques/error-logger-delivery-module.git
